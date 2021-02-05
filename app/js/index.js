0ec61484a1a7d8c642e74bfffe14d61ebc638a71
Wanderson Silva <wandersonsilva768@gmail.com>
2021-02-05 05:34:45 -0300

    feat: finalizada header responsiva, dark mode funcionando

---
+++ app/js/index.js
@@ -0,0 +1,13 @@
+(function () {
+
+  const $ = document.querySelector.bind(document);
+
+  const toggleBox = $('input#themetoggle');
+
+  toggleBox.addEventListener('input', e => {
+    e.preventDefault();
+
+    document.body.classList.toggle('dark', e.target.checked);
+  }, false);
+
+})();
